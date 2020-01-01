07c36ed83d642ad22d877389747bb03a3aa2444d
CharityParks <tuppercharity@gmail.com>
2019-12-31 23:50:26 -0500

    added UserController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
     # users can sign up
     get '/signup' do
-        erb :'users/signup'
+        erb :'/users/signup'    #  /users/signup?  or without the slash before user?
       end
   
       post '/signup' do
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
           user = User.new(params)  #instantiates a new user
           if user.username.empty? || user.password.empty?
               @error = "Please enter a valid username and password."
-              erb :'users/signup'
+              erb :'/users/signup'        #  here too
           else
               user.save
               session[:user_id] = user.id   #this line is logging them in
